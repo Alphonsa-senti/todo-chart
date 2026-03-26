0cc0f2b2270a9de7af8130ecf511b7ccd3c55678
Alphonsa <alphonsa.divyanathan@sentinelfox.com>
2026-03-26 22:13:55 +0530

    update

--- src/test/java/com/example/ExampleTest.java
+++ src/test/java/com/example/ExampleTest.java
@@ -1,12 +1,29 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testSuccess() {
+        assertEquals(2, 1 + 1); // This will Pass
+    }
+
+    @Test
+    void testFailure() {
+        assertEquals(10, 1 + 1); // This will Fail (Assertion failure)
+    }
+
+    @Test
+    @Disabled("Testing skip count")
+    void testSkipped() {
+        // This will be Skipped
+    }
+
+    @Test
+    void testError() {
+        throw new RuntimeException("Crashed!"); // This counts as an Error
     }
 }
