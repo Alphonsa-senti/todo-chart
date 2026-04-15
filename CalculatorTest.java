b1e448373118ef586a8354c70a719d15c5cc3a01
Alphonsa <alphonsa.divyanathan@sentinelfox.com>
2026-04-15 19:14:09 +0530

    update

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,36 +1,29 @@
+package com.example;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 public class ExampleTest {
 
     @Test
     void testSuccess() {
-        assertEquals(2, 1 + 1); // This will PASS
+        assertEquals(2, 1 + 1); // This will Pass
     }
 
     @Test
     void testFailure() {
-        assertEquals(5, 1 + 1); // This will FAIL (AssertionError)
+        assertEquals(10, 1 + 1); // This will Fail (Assertion failure)
     }
 
-    @Test
-    void testError() {
-        assertThrows(RuntimeException.class, () -> {
-            throw new RuntimeException("Crash!");
-        });
-    }
-    
     @Test
     @Disabled("Testing skip count")
     void testSkipped() {
-        // This will be SKIPPED because of @Disabled
+        // This will be Skipped
     }
 
     @Test
     void testError() {
-        throw new RuntimeException("Crash!"); // This is an ERROR (Exception)
+        throw new RuntimeException("Crashed!"); // This counts as an Error
     }
 }
