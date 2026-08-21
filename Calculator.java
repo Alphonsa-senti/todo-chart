368c3022b4e7069539c81f47ad36ffeda5b2d682
Alphonsa <alphonsa.divyanathan@sentinelfox.com>
2026-08-21 15:50:36 +0530

    simple calculator app

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculato {
+
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
