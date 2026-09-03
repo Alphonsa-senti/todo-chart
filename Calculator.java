df318490e722a38fbc66152fb8470a8c60af8f1f
SRE Agent <sre-agent@yourcompany.com>
2026-09-03 07:16:00 +0000

    fix: auto-generated application fix for infra in java-calculator-89cf576b4-h67nw

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        String value = null;
+        String value = "";
 
         System.out.println("Value length: " + value.length());
 
